137cb9e54add1e50ed8d590442330f2d7bf46dfc
Roshin C <idroshin2010@gmail.com>
2017-06-13 16:30:20 -0400

    Better Docs

--- src/externs.h
+++ src/externs.h
@@ -1,3 +1,3 @@
-extern SdFat sd;
-extern bool initilized;
-extern ApplicationWatchdog wd;
+extern SdFat sd; //the sd card
+extern bool initilized; // true if sd was initilized
+extern ApplicationWatchdog wd; // system reboot watchdog
--- src/globals.h
+++ src/globals.h
@@ -1,4 +1,4 @@
-SdFat sd;
-bool initilized = false;
+SdFat sd; //the sd card
+bool initilized = false; // true if sd was initilized
 // reset the system after 60 seconds if the application is unresponsive
-ApplicationWatchdog wd(60000, System.reset);
+ApplicationWatchdog wd(60000, System.reset); // system reboot watchdog
